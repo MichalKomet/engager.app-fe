0e6d5a4583ec150bd93a34ffbc93370815b3416f
Michał Gwoździej <gwozdziej.michal@gmail.com>
2025-01-22 22:37:44 +0100

    create Home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,81 @@
+import { useEffect, useState } from 'react';
+import { Button, Container, Typography } from '@mui/material';
+import { getAllTodos, createTodo, updateTodo, deleteTodo } from '../services/api';
+import TodoList from '../components/TodoList';
+import TodoDialog from '../components/TodoDialog';
+import { Todo } from '../types';
+
+export default function Home() {
+    const [todos, setTodos] = useState<Todo[]>([]);
+    const [dialogOpen, setDialogOpen] = useState(false);
+    const [editingTodo, setEditingTodo] = useState<Todo | undefined>(undefined);
+
+    useEffect(() => {
+        fetchTodos();
+    }, [dialogOpen]);
+
+    const fetchTodos = async () => {
+        try {
+            const data = await getAllTodos();
+            setTodos(data);
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    const handleDelete = async (id: number) => {
+        try {
+            await deleteTodo(id);
+            setTodos((prev) => prev.filter((t) => t.id !== id));
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    const handleEdit = (todo: Todo) => {
+        setEditingTodo(todo);
+        setDialogOpen(true);
+    };
+
+    const handleOpenNew = () => {
+        setEditingTodo(undefined);
+        setDialogOpen(true);
+    };
+
+    const handleSave = async (data: Partial<Todo>) => {
+        try {
+            if (editingTodo) {
+                const updated = await updateTodo(editingTodo.id, data);
+                setTodos((prev) =>
+                    prev.map((t) => (t.id === editingTodo.id ? updated : t))
+                );
+            } else {
+                const newTodo = await createTodo(data);
+                setTodos((prev) => [...prev, newTodo]);
+            }
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    return (
+        <Container sx={{ mt: 4 }}>
+            <Typography variant="h4" gutterBottom>
+                My Great TODO List
+            </Typography>
+
+            <Button variant="contained" onClick={handleOpenNew} sx={{ mb: 2 }}>
+                Add New
+            </Button>
+
+            <TodoList todos={todos} onEdit={handleEdit} onDelete={handleDelete} />
+
+            <TodoDialog
+                open={dialogOpen}
+                onClose={() => setDialogOpen(false)}
+                onSave={handleSave}
+                editingTodo={editingTodo}
+            />
+        </Container>
+    );
+}
